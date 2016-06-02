4c8ad456a008c1808b5321a2e879f4cc713e96f1
Reece Bennett <reece.bennett1@gmail.com>
2016-06-02 21:36:18 +0100

    Add game logic to main.ts

--- output.js
+++ output.js
@@ -1,13 +1,47 @@
+// Global variables
+var players;
+var startMoney = 100;
+var bigBlind = 2;
 /**
  * Player
  */
 var Player = (function () {
-    function Player(name) {
+    function Player(name, money) {
         this.name = name;
-        this.cash = 1000;
+        this.money = money;
     }
+    Player.prototype.pay = function (amount) {
+        this.money -= amount;
+        this.inCurrentPot += amount;
+    };
+    Player.prototype.choose = function () {
+        console.log(this.name + " choose check, raise or fold");
+    };
     return Player;
 }());
+function play(round) {
+    // Make players pay big-blind and little-blind
+    var firstPlayer = round % players.length;
+    players[firstPlayer].pay(bigBlind / 2);
+    players[firstPlayer + 1].pay(bigBlind);
+    // Phases are 0: hand cards, 1: flop, 2: turn, 3: river (finished)
+    var phase = 0;
+    var potTotal = 0;
+    var potPerPlayer = 0;
+    while (phase <= 3) {
+        var nextPlayer = 0;
+        var playersReady = 0;
+        var curPlayers = players;
+        while (playersReady < curPlayers.length) {
+            curPlayers[nextPlayer].choose();
+            nextPlayer++;
+            if (nextPlayer >= curPlayers.length) {
+                nextPlayer = 0;
+            }
+        }
+        phase++;
+    }
+}
 window.onload = function () {
     console.log("Hello world!");
 };
--- ts/main.ts
+++ ts/main.ts
@@ -2,7 +2,6 @@
 let players: Player[];
 let startMoney = 100;
 let bigBlind = 2;
-let potAmount = 0;
 
 /**
  * Player
@@ -11,15 +10,44 @@ class Player {
     
     name: string;
     money: number;
+    inCurrentPot: number;
     
     constructor(name: string, money: number) {
         this.name = name;
         this.money = money;
     }
+    
+    pay(amount: number) {
+        this.money -= amount;
+        this.inCurrentPot += amount;
+    }
+    
+    choose() {
+        console.log(this.name + " choose check, raise or fold");
+    }
 }
 
 function play(round: number) {
+    // Make players pay big-blind and little-blind
+    let firstPlayer = round % players.length;
+    players[firstPlayer].pay(bigBlind / 2);
+    players[firstPlayer + 1].pay(bigBlind);
     
+    // Phases are 0: hand cards, 1: flop, 2: turn, 3: river (finished)
+    let phase = 0;
+    let potTotal = 0;
+    let potPerPlayer = 0;
+    while (phase <= 3) {
+        let nextPlayer = 0;
+        let playersReady = 0;
+        let curPlayers = players;
+        while (playersReady < curPlayers.length) {
+            curPlayers[nextPlayer].choose();
+            nextPlayer++;
+            if (nextPlayer >= curPlayers.length) { nextPlayer = 0 } 
+        }
+        phase++;
+    }
 }
 
 window.onload = function() {
