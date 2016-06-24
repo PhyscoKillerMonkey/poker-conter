0e068ff5fc4f3a8582ff950870c700643cb44df7
Reece Bennett <reece.bennett1@gmail.com>
2016-06-24 22:52:48 +0100

    Start splitting server code into modules

---
+++ src/server/poker.ts
@@ -0,0 +1,162 @@
+export class Player {
+  name: string;
+  id: string;
+  money: number;
+  inCurrentPot: number;
+  folded: boolean;
+  played: boolean;
+
+  constructor(name: string, id: string, money: number) {
+    this.name = name;
+    this.id = id;
+    this.money = money;
+    this.inCurrentPot = 0;
+    this.folded = false;
+    this.played = false;
+  }
+
+  pay(amount: number) {
+    this.money -= amount;
+    this.inCurrentPot += amount;
+    // potTotal += amount;
+  }
+
+  ready(potPP: number): boolean {
+    if (this.folded || (this.played && this.inCurrentPot == potPP)) {
+      return true
+    } else if (this.inCurrentPot > potPP) {
+      console.error("Player " + this.name + " has more money in pot than total...");
+    } else {
+      return false;
+    }
+  } 
+}
+
+export class PokerManager {
+  private startingMoney: number;
+  private bigBlind: number;
+  private players: Player[] = [];
+  private currentPlayer = 0;
+  private dealer = -1;
+  private potTotal = 0;
+  private potPP = 0;
+  private round = 0;
+  private phase = 0;
+  private room;
+
+  constructor(room, startingMoney?: number, bigBlind?: number) {
+    this.room = room;
+    this.startingMoney = startingMoney || 500;
+    this.bigBlind = bigBlind || 10;
+  }
+
+  private nextPlayer() {
+    this.currentPlayer++;
+    if (this.currentPlayer >= this.players.length) {
+      this.currentPlayer = 0;
+    }
+    if (this.players[this.currentPlayer].folded) {
+      this.nextPlayer();
+    }
+  }
+
+  private allReady(): boolean {
+    for (let p of this.players) {
+      if (!p.ready(this.potPP)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private numFolded(): number {
+    let f = 0;
+    for (let p of this.players) {
+      if (p.folded) {
+        f++;
+      }
+    }
+    return f;
+  }
+
+  raise(amount: number) {
+    this.potPP += amount;
+    console.log(this.potPP);
+    let p = this.players[this.currentPlayer];
+    p.pay(this.potPP - p.inCurrentPot);
+    p.played = true;
+    this.nextPlayer();
+    this.doTurn();
+  }
+
+  fold() {
+    this.players[this.currentPlayer].folded = true;
+    this.nextPlayer();
+    this.doTurn();
+  }
+
+  newRound() {
+    this.round++;
+    this.phase = 0;
+    this.potTotal = 0;
+    this.potPP = this.bigBlind;
+
+    for (let p of this.players) {
+      p.folded = false;
+      p.inCurrentPot = 0;
+      p.played = false;
+    }
+
+    // Make the dealer one more, then the player one more than him
+    this.currentPlayer = this.dealer;
+    this.nextPlayer();
+    this.dealer = this.currentPlayer;
+    console.log(this.players[this.dealer].name + " is dealer");
+    this.nextPlayer();
+    console.log(this.players[this.currentPlayer].name + " is SB");
+    this.players[this.currentPlayer].pay(this.bigBlind/2);
+    this.nextPlayer();
+    console.log(this.players[this.currentPlayer].name + " is BB");
+    this.players[this.currentPlayer].pay(this.bigBlind);
+    this.nextPlayer();
+    console.log(this.players[this.currentPlayer].name + " is UTG");
+
+    this.doTurn();
+  }
+
+  private doTurn() {
+    if (this.numFolded() == this.players.length - 1) {
+      console.log("Only one player left");
+      // Find the remaining player and make them the winner
+      for (let p of this.players) {
+        if (!p.folded) {
+          this.winnerIs(p);
+        }
+      }
+    } else if (this.phase == 4) {
+      console.log("The game has ended, " + this.players[0].name + " is choosing a winner");
+      this.room.updateClients();
+      this.room.clientMessage(this.players[0], "chooseWinner");
+    } else if (this.allReady()) {
+      // Go into the next phase
+      this.phase++;
+      console.log("Everybody is ready, going into phase " + this.phase);
+      this.currentPlayer = this.dealer;
+
+      for (let p of this.players) {
+        p.played = false;
+      }
+      this.doTurn();
+    } else {
+      this.room.updateClients();
+      console.log("Player " + this.players[this.currentPlayer].name + " has to choose");
+      this.room.clientMessage(this.players[this.currentPlayer], "choose");
+    }
+  }
+
+  private winnerIs(player: Player) {
+    console.log("Winner is " + player.name);
+    player.money += this.potTotal;
+    this.newRound();
+  }
+}
--- src/server/server.ts
+++ src/server/server.ts
@@ -2,10 +2,13 @@
 
 import express = require("express");
 let app = express();
+
 import http = require("http");
 let server = http.createServer(app);
 import socketio = require("socket.io");
 let io = socketio(server);
+
+import { Player, PokerManager } from "./poker.ts";
 
 // Setup the public folder for the web server
 app.use(express.static("public"));
@@ -16,297 +19,36 @@ app.get("/", function(req, res) {
     if (err) {
       console.error(err);
       res.status(err.status).end();
-    } else {
-      console.log("Sent: " + filename);
     }
   });
 });
+
+
+
+let users: {[name: string]: {id: string, socket: SocketIO.Socket, room: string}} = {};
 
 io.on("connection", function(socket) {
-  let me: Player;
-  console.log(socket.id + " connected");
+  console.log("ID " + socket.id + " is connecting...");
+  let userName = undefined;
+  // updateRooms(socket);
 
-  function myTurn(): boolean {
-    if (players[currentPlayer] == me) {
-      return true;
+  socket.on("getName", function(data) {
+    if (!users[data.name]) {
+      users[data.name] = { id: socket.id, socket: socket, room: undefined }
+      userName = data.name;
+      socket.emit("nameStatus", { avaliable: true });
     } else {
-      return false;
-    }
-  }
-
-  socket.on("join", function(name: string) {
-    let taken = false;
-    for (let p of players) {
-      if (p.name == name) {
-        taken = true;
-      }
-    }
-    if (me == undefined && !taken) {
-      console.log(name + " joined, ID: " + socket.id);
-      me = new Player(name, socket.id);
-      players.push(me);
-      updateClients();
-      socket.emit("nameAvaliable")
-    } else {
-      console.log("Name: " + name + ", not avaliable");
-      socket.emit("nameTaken");
+      socket.emit("nameStatus", { avaliable: false });
     }
   });
 
-  socket.on("startGame", function() {
-    if (players.length >= 2) {
-      newRound();
-    } else {
-      console.log("Not enough players to start");
+  socket.on("joinRoom", function(data) {
+    if (users[userName].room == undefined) {
+      socket.join(data.room);
+      users[userName].room = data.room;
+      // updateRooms();
     }
-  });
-
-  socket.on("check", function() {
-    if (myTurn()) {
-      console.log(me.name + " checked");
-      check();
-    } else {
-      console.log("Not " + me.name + "'s turn");
-    }
-  });
-
-  socket.on("raise", function(amount: string) {
-    if (myTurn()) {
-      console.log(me.name + " raised £" + amount);
-      raise(parseInt(amount));
-    } else {
-      console.log("Not " + me.name + "'s turn");
-    }
-  });
-
-  socket.on("fold", function() {
-    if (myTurn()) {
-      console.log(me.name + " folded");
-      fold();
-    } else {
-      console.log("Not " + me.name + "'s turn");
-    }
-  });
-
-  socket.on("winnerIs", function(id: string) {
-    for (let p of players) {
-      if (p.id == id) {
-        winnerIs(p);
-      }
-    }
-  });
-
-  socket.on("disconnect", function() {
-    console.log(socket.id + " disconnected");
-    players.splice(players.indexOf(me));
-    me = undefined;
-    updateClients();
   });
 });
 
-server.listen(3000, function() {
-  console.log("Listening on port 3000");
-});
-
-function updateClients() {
-  io.emit("update", new updateObject());
-}
-
-function clientMessage(player: Player, msg: string) {
-  io.to(player.id).emit(msg);
-}
-
-// Object to pass to the clients when the game state changes
-class updateObject {
-  players: Player[];
-  currentPlayer: number;
-  dealer: number;
-  potTotal: number;
-  potPP: number;
-  round: number;
-  phase: number;
-
-  constructor() {
-    this.players = players;
-    this.currentPlayer = currentPlayer;
-    this.dealer = dealer;
-    this.potTotal = potTotal;
-    this.potPP = potPP;
-    this.round = round;
-    this.phase = phase;
-  }
-}
-
-
-
-/*************************************/
-/*          Poker Game Code          */
-/*************************************/
-
-let startingMoney = 100,
-  bigBlind = 4;
-
-let players: Player[] = [],
-  currentPlayer = 0,
-  dealer = -1,
-  potTotal = 0,
-  potPP = 0,
-  round = 0,
-  phase = 0;
-
-class Player {
-  name: string;
-  id: string;
-  money: number;
-  inCurrentPot: number;
-  folded: boolean;
-  played: boolean;
-
-  constructor(name: string, id: string, money?: number) {
-    this.name = name;
-    this.id = id;
-    this.money = money || startingMoney;
-    this.inCurrentPot = 0;
-    this.folded = false;
-    this.played = false;
-  }
-
-  pay(amount: number) {
-    this.money -= amount;
-    this.inCurrentPot += amount;
-    potTotal += amount;
-  }
-
-  ready(): boolean {
-    if (this.folded || (this.played && this.inCurrentPot == potPP)) {
-      return true
-    } else if (this.inCurrentPot > potPP) {
-      console.error("Player " + this.name + " has more money in pot than total...");
-    } else {
-      return false;
-    }
-  } 
-}
-
-function nextPlayer() {
-  currentPlayer++;
-  if (currentPlayer >= players.length) {
-    // console.log("Out of bounds with " + currentPlayer);
-    currentPlayer = 0;
-  }
-  if (players[currentPlayer].folded) {
-    // console.log(currentPlayer + " folded, skipping");
-    nextPlayer();
-  }
-}
-
-// Problem may be caused when someone is folded
-function allReady(): boolean {
-  for (let p of players) {
-    if (!p.ready()) {
-      return false;
-    }
-  }
-  return true;
-}
-
-function folded(): number {
-  let f = 0;
-  for (let p of players) {
-    if (p.folded) {
-      f++;
-    }
-  }
-  return f;
-}
-
-function check() {
-  let p = players[currentPlayer];
-  p.pay(potPP - p.inCurrentPot);
-  p.played = true;
-  nextPlayer();
-  doTurn();
-}
-
-function raise(amount: number) {
-  console.log(potPP);
-  console.log(amount);
-  potPP += amount;
-  console.log(potPP);
-  let p = players[currentPlayer];
-  p.pay(potPP - p.inCurrentPot);
-  p.played = true;
-  nextPlayer();
-  doTurn();
-}
-
-function fold() {
-  players[currentPlayer].folded = true;
-  nextPlayer();
-  doTurn();
-}
-
-function newRound() {
-  round++;
-  phase = 0;
-  potTotal = 0;
-  potPP = bigBlind;
-
-  for (let p of players) {
-    p.folded = false;
-    p.inCurrentPot = 0;
-    p.played = false;
-  }
-
-  // Make the dealer one more, then the player one more than him
-  currentPlayer = dealer;
-  nextPlayer();
-  dealer = currentPlayer;
-  console.log(players[dealer].name + " is dealer");
-  nextPlayer();
-  console.log(players[currentPlayer].name + " is SB");
-  players[currentPlayer].pay(bigBlind/2);
-  nextPlayer();
-  console.log(players[currentPlayer].name + " is BB");
-  players[currentPlayer].pay(bigBlind);
-  nextPlayer();
-  console.log(players[currentPlayer].name + " is UTG");
-
-  doTurn();
-}
-
-function doTurn() {
-  if (folded() == players.length - 1) {
-    console.log("Only one player left");
-    // Find the remaining player and make them the winner
-    for (let p of players) {
-      if (!p.folded) {
-        winnerIs(p);
-      }
-    }
-  } else if (phase == 4) {
-    console.log("The game has ended, " + players[0].name + " is choosing a winner");
-    updateClients();
-    clientMessage(players[0], "chooseWinner");
-  } else if (allReady()) {
-    // Go into the next phase
-    phase++;
-    console.log("Everybody is ready, going into phase " + phase);
-    currentPlayer = dealer;
-
-    for (let p of players) {
-      p.played = false;
-    }
-    doTurn();
-  } else {
-    updateClients();
-    console.log("Player " + players[currentPlayer].name + " has to choose");
-    clientMessage(players[currentPlayer], "choose");
-  }
-}
-
-function winnerIs(player: Player) {
-  console.log("Winner is " + player.name);
-  player.money += potTotal;
-  newRound();
-}
+function roomUpdate(socket: SocketIO.Socket)
